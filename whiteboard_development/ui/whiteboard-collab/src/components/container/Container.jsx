82576fbabeb3262f2747193b21c4dd6776cc7821
adi <akumatha@gmail.com>
2021-12-07 19:37:29 +0530

    second files

--- whiteboard_development/ui/whiteboard-collab/src/components/container/Container.jsx
+++ whiteboard_development/ui/whiteboard-collab/src/components/container/Container.jsx
@@ -28,41 +28,37 @@ class Container extends React.Component {
     render() {
 
         return (
+
+       
             <div className="container">
-                <h3 className="name-midile">Development of a Distributed whiteboard Application</h3>
+
+              
+
+
+          
+               <h3 className="name-midile">Explain Everything Online Whiteboard</h3>
                 <div class="tools-section">
                     <div className="color-picker-container">
                         Select Brush Color : &nbsp;
                         <input type="color" value={this.state.color} onChange={this.changeColor.bind(this)} />
                     </div>
-
+<br />
                     <div className="brushsize-container">
                         Select Brush Size : &nbsp;
                         <select value={this.state.size} onChange={this.changeSize.bind(this)}>
-                            <option> 2</option>
-                            <option> 4 </option>
-                            <option>  6</option>
-                            <option> 8 </option>
-                            <option>  10</option>
-                            <option> 12 </option>
-                            <option> 14 </option>
-                            <option> 16 </option>
-                            <option>  18</option>
-                            <option>  20</option>
-                            <option>  22</option>
-                            <option>  24</option>
-                            <option> 26</option>
-                            <option>  28</option>
-                            <option>  30</option>
-                            <option>  32</option>
-                            <option>  34</option>
-                            <option>  36</option>
-                            <option> 38</option>
-                            <option>  40</option>
-                            <option>  42</option>
-                            <option>  44</option>
-                            <option> 48</option>
-                            <option>  48</option>
+                           <option>1</option>
+                           <option>5</option>
+                           <option>10</option>
+                           <option>15</option>
+                           <option>20</option>
+                           <option>25</option>
+                           <option>30</option>
+                           <option>35</option>
+                           <option>40</option>
+                           <option>45</option>
+                           <option>50</option>
+                           <option>55</option>
+
                         </select>
                     </div>
 
